ac30358675d3e6fe68f1db3249ba9d7de85fae0f
mnwana <mariellenwana@gmail.com>
2022-07-17 15:45:36 -0400

    add functions structure for js

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+var history = [];
+var apiKey = 'a8f5376058c57b89eb2148315aa2f653';
+var submitBtn = document.getElementById("submit-btn");
+
+// load search history from local storage
+var loadHistory = function() {
+    
+};
+
+// save search history to local storage
+var saveHistory = function(){
+
+};
+
+// initialize page on load
+var initializePage = function(){
+
+};
+
+// get weather data for a city from API
+var getApiData = function(cityName){
+
+};
+
+// create history button for a city
+var createHistoryButton = function(cityName){
+
+};
+
+// get API data for a city, load API Data to page, add search to history
+var submitBtnHandler = function(event){
+
+};
+
+// event listener for submit search button
+submitBtn.addEventListener("submit",submitBtnHandler);
+
+
+// load history from local storage and initialize page
+loadHistory();
+initializePage();
